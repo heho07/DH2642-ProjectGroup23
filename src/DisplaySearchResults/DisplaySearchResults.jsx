776146815b290e02b3731d36dbd13b5e9400581b
kirschberg <davidkirsch5@gmail.com>
2019-04-09 18:38:28 +0200

    cleaning up code

--- src/DisplaySearchResults/DisplaySearchResults.jsx
+++ src/DisplaySearchResults/DisplaySearchResults.jsx
@@ -159,59 +159,6 @@ class SearchResults extends Component {
                           </tr>
                         </tbody>
                       </table>
-                    
-                    <table>
-                        <tbody>
-                          <tr>
-                            <td>
-                              <b>Name</b>
-                            </td>
-                            <td>
-                              {item.name}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <b>Card ID</b>
-                            </td>
-                            <td>
-                              {item.cardId}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <b>type </b>
-                            </td>
-                            <td>
-                              {item.type}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <b>cost </b>
-                            </td>
-                            <td>
-                              {item.cost}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <b>attack </b>
-                            </td>
-                            <td>
-                              {item.attack}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <b>health </b>
-                            </td>
-                            <td>
-                              {item.health}
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
                       <br />
                       <button className="btn btn-dark" id="gameButton" onClick = {() => this.handleClickOnCard(item, "user")}>Add to user</button> <br />
                       <button className="btn btn-dark" id="gameButton" onClick = {() => this.handleClickOnCard(item, "opponent")}>Add to opponent</button><br />
