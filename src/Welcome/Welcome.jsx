d224c913b715edb88b5e633f7ae741138eff4104
kirschberg <davidkirsch5@gmail.com>
2019-04-12 21:05:20 +0200

    Added images

--- src/Welcome/Welcome.jsx
+++ src/Welcome/Welcome.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Welcome.css";
 
+
 class Welcome extends Component {
   
     render() {
@@ -11,12 +12,14 @@ class Welcome extends Component {
         <center>
           <div className = "row">
             <div className = "col-sm-4 teststyle">
-              <center>Add image here</center>
+              
               <center>
-
-              <p className ="imageParagraph">Image should be clickable</p>
+              <img className="imageOptionCollection" src={require('./collection_small.png')} />
+            
               </center>
               <br />
+              <p className ="imageParagraph">Check your cards</p>
+            
               <h4>
               <center>
               <Link to="/MyCollection">
@@ -27,11 +30,14 @@ class Welcome extends Component {
               </h4>
             </div> 
             <div className = "col-sm-4 teststyle">
-              <center>Add image here</center>
               <center>
-              <p className ="imageParagraph"> Image should be clickable</p>
+                <img className="imageOptionVersus" src={require('./versus_small.png')} />
               </center>
-              <br />            
+              <center>
+              <br/>
+              <p className ="imageParagraph"> Battle other cards</p>
+              </center>
+                         
               <h4><center> 
               <Link to = "/ChooseDifficulty">
                 <button type="button" className="btn btn-dark">VS mode</button>
@@ -39,11 +45,13 @@ class Welcome extends Component {
               </center></h4>
             </div> 
             <div className = "col-sm-4 teststyle">
-              <center>Add image here</center>
               <center>
-              <p className ="imageParagraph"> Image should be clickable</p>
+                <img className="imageOptionCoins" src={require('./coins_small.png')} />
               </center>
-              <br />
+              <center>
+              <br/>
+              <p className ="imageParagraph"> Buy new cards</p>
+              </center>
               <h4><center>
               <Link to="/SearchScreen">
                 <button type="button" className="btn btn-dark">Store</button>
