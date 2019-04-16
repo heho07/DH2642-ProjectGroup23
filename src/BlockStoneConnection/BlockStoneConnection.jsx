63f67f54e652ebcf13a5e48949855a12239110c6
Jy-Liu <junyouliu9@gmail.com>
2019-04-16 23:08:26 +0200

    get nameIds and prices

--- src/BlockStoneConnection/BlockStoneConnection.jsx
+++ src/BlockStoneConnection/BlockStoneConnection.jsx
@@ -22,7 +22,7 @@ class BlockStoneConnection extends Component {
  //      <button id="btn1" data-id="1" value="2000000000000000000">name1</button>
  //      <button id="btn2" data-id="2" value="3000000000000000000">name2</button>
  //    </div>
- 
+
 // The HTML to be rendered on the page
   handleClick(){
     let res = window.ConnectClass.getInfo(window.ConnectClass.contract, window.ConnectClass.account );
@@ -34,15 +34,53 @@ class BlockStoneConnection extends Component {
       <div>
         <button onClick = {() => this.handleClick()}>ANROPAR CONNECT</button>
         <button onClick = {() => modelInstance.getAllCardsFromAPI().then((res) => {
-            console.log(res);
             let arr = [];
-            Object.values(res).forEach( (i) =>{
-              i.forEach((j) => arr.push(j));
+            Object.values(res).forEach((category) =>{
+              category.forEach((card) => {
+                if (card.cardId &&
+                  card.cardId !== undefined &&
+                  card.cardId.length <= 16 &&
+                  card.attack &&
+                  card.attack !== undefined &&
+                  card.health &&
+                  card.health !== undefined &&
+                  card.rarity &&
+                  card.rarity !== undefined &&
+                  card.name &&
+                  card.name !== undefined) {
+                  arr.push(card);
+                }
+              });
             });
-            
 
+            console.log("card number: "+arr.length);
 
-          })}>all cards</button>
+            const rarity = {
+              "Free": 1,
+              "Common": 2,
+              "Rare": 3,
+              "Epic": 4,
+              "Legendary": 5
+            }
+            let nameIds = [];
+            let prices = [];
+
+            arr.forEach((card) => {
+              // push cardId
+              let id = window.web3.utils.padRight(window.web3.utils.asciiToHex(card.cardId), 32);
+              nameIds.push(id);
+              // calculate price and push it
+              let price =  3*card.health + 2*card.attack + 10*rarity[card.rarity];
+              prices.push(window.web3.utils.toWei(String(price)));
+            });
+
+            console.log(nameIds);
+            console.log(nameIds.length);
+            console.log(prices);
+            console.log(prices.length);
+            console.assert(prices.length === nameIds.length);
+          })
+        }>all cards</button>
       </div>
     );
   }
